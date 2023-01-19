4d9c06767aba95858ed1c5b24754ccb71597ed51
bbiyagi <bulehychoi@naver.com>
2023-01-19 22:34:44 +0900

    계좌조회 스타트

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,43 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <div class="about"></div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      dd: "22",
+    };
+  },
+  created() {
+    console.log();
+  },
+  mounted() {
+    this.getAccountInfo();
+  },
+  methods: {
+    getAccountInfo() {
+      axios
+        .get("https://openapi.openbanking.or.kr/oauth/2.0/authorize", {
+          hearders: {
+            "Access-Control-Allow-Origin": "http://localhost:8080/",
+          },
+          response_type: "code",
+          client_id: "f9187864-bcf8-4cfe-a1c6-d1060be387e3",
+          redirect_uri: "http://localhost:8080/account",
+          scope: "login inquiry transfer",
+          state: "231312983190481241842",
+          auth_type: "0",
+        })
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
+</script>
